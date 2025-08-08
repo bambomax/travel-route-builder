56950459b85250d0f4e410f9151a68bd26e83ae7
monyshchenko <monyshchenko@shutterstock.com>
2025-08-08 19:06:53 +0300

    add Handle to CountryNode

--- src/Components/Nodes/CountryNode.tsx
+++ src/Components/Nodes/CountryNode.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 import type { Node, NodeProps } from '@xyflow/react';
+import { Handle, Position } from '@xyflow/react';
 
 import type { Country } from '../../types';
 import styles from './CountryNode.module.css';
@@ -9,6 +10,10 @@ const CountryNode: FC<NodeProps<Node<Country>>> = ({ data }) => {
 
   return (
     <div className={styles.country}>
+      <Handle type="target" position={Position.Top} />
+      <Handle type="source" position={Position.Bottom} id="a" style={{ left: '25%' }} />
+      <Handle type="source" position={Position.Bottom} id="b" style={{ left: '75%' }} />
+
       <div className={styles.flagWrapper}>
         <img src={flags.svg} alt={flags.alt} className={styles.countryFlag} />
       </div>
